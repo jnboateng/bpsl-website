7e98e9434378a5b4a4409955b2c91d28aab0f36f
Laudkyle <kyleaby1@gmail.com>
2025-06-10 13:46:48 +0000

    banner

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Hero from '../components/About/Hero'
 import AboutSection from '../components/About/AboutSection'
 import AwardList from '../components/About/AwardList'
-
-const heroBg="https://res.cloudinary.com/dinb6qtto/image/upload/v1747327037/fuelme/eunqurz5ywlilv9qris7.png"
+import banner from "../images/logo/hero-banner.svg"
+const heroBg=""
 
 function About() {
   return (
     <div>
-      <Hero image={heroBg} text1={"about us"}/>
+      <Hero image={banner} text1={"about us"}/>
       <AboutSection />
     </div>
   )
