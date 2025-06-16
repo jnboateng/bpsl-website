d32095fb99f83e66ff8b8bfeb11ec6350512650a
Laudkyle <kyleaby1@gmail.com>
2025-06-16 13:16:34 +0000

    j

--- src/components/careers/Describe.jsx
+++ src/components/careers/Describe.jsx
@@ -22,7 +22,7 @@ function Describe() {
           </div>
         </div>
 
-        <div className="w-full flex justify-center md:mt-[24rem] mt-[20rem]">
+        <div className="w-full flex justify-center md:mt-[24rem] mt-[10rem]">
           <button
             onClick={() => {
               document
