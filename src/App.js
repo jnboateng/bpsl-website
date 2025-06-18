ddc89c95844ee7fac965191b748dff4971778aea
Laudkyle <kyleaby1@gmail.com>
2025-06-18 13:18:04 +0000

    chanfge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter} from "react-router-dom";
 import Homepage from "./Pages/Homepage";
-
+import { HelmetProvider } from "react-helmet-async";
 
 const App = () => {
  
   return (
+
+    
     <BrowserRouter>
       <Homepage />
     </BrowserRouter>
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,23 @@
-import Hero from '../components/About/Hero'
-import AboutSection from '../components/About/AboutSection'
-import banner from "../images/logo/hero-banner.svg"
+import Hero from "../components/About/Hero";
+import AboutSection from "../components/About/AboutSection";
+import banner from "../images/logo/hero-banner.svg";
+import { Helmet } from "react-helmet-async";
 
 function About() {
   return (
     <div>
-      <Hero image={banner} text1={"about us"}/>
+      <Helmet>
+        <title>About Best Point Savings & Loans – Licensed Financial Institution in Ghana</title>
+        <meta
+          name="description"
+          content="Learn about Best Point’s mission, values, leadership, and regulatory licensing as a trusted Ghanaian savings and loans company."
+        />
+      </Helmet>
+
+      <Hero image={banner} text1={"about us"} />
       <AboutSection />
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
