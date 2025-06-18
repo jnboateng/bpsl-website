673091cb850ff2c8605d4d9287d64dd5ebdc78db
Laudkyle <kyleaby1@gmail.com>
2025-06-18 14:02:27 +0000

    news

--- src/Pages/Stories.jsx
+++ src/Pages/Stories.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Hero from '../components/About/Hero'
 import StoriesArticlesGallery from '../components/Stories/Articles'
+import { Helmet } from 'react-helmet-async'
 
 const heroBg ="https://res.cloudinary.com/dinb6qtto/image/upload/v1747327033/fuelme/tlimnbsaqc9mweziayjl.png"
 function Stories() {
   return (
     <div>
+      <Helmet>
+        <title>News – Tips, Articles, Stories & News from Best Point
+</title>
+<meta name='description' content='Stay updated with the latest news and announcements from Best Point. Explore stories on financial services, community initiatives, and business growth.' />
+      </Helmet>
       <Hero image={heroBg} text1={"Stories"}/>
       <StoriesArticlesGallery />
     </div>
