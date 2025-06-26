c032390bc16538d6c846af8a7d55f79838e0b079
Laudkyle <kyleaby1@gmail.com>
2025-06-26 12:23:18 +0000

    url

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://e19e-41-191-99-98.ngrok-free.app/api",
+  baseURL: "https://0ba8-41-191-99-98.ngrok-free.app/api",
   headers: {
     "Content-Type": "application/json",
   },
