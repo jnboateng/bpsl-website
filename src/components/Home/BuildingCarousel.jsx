65cd7dca862f876bc953b8dffe0fd916c020068d
Laudkyle <kyleaby1@gmail.com>
2025-08-01 09:36:52 +0000

    changes

--- src/components/Home/BuildingCarousel.jsx
+++ src/components/Home/BuildingCarousel.jsx
@@ -78,7 +78,7 @@ export default function BuildingCarousel() {
             <h2 className="text-xl font-bold text-gray-800">{title}</h2>
             <p className="text-gray-700">{description}</p>
             <p className="font-medium text-gray-700">{amount}</p>
-            <Link to={linkedPage} className="mt-3 bg-purple-200 hover:bg-purple-100 text-white px-4 py-2 rounded-lg">
+            <Link to={linkedPage} className="mt-6 bg-purple-200 hover:bg-purple-100 text-white px-4 py-2 rounded-lg">
               {buttonText}
             </Link>
           </div>
